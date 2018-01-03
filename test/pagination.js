4b1d62bb4cbd8566de642cc0cce42d9e60fda612
Matteo 'Peach' Pescarin <peach@smartart.it>
2018-01-03 18:42:23 +0000

    added more tests for Page object
    - updated package-lock.json

--- test/pagination.js
+++ test/pagination.js
@@ -17,12 +17,30 @@ t.test('Pagination object', function (t) {
 });
 
 t.test('Page object', function (t) {
-  t.skip();
+  const layout = 'layout';
+  const groupName = 'group';
+  const path = 'path/to/file/';
+  const filename = 'filename';
+  const extension = '.html';
+  const description = 'a nice description';
+  const pages = [ 'page1', 'page2', 'page3' ];
+  const files = [ {title: 'something'}, {title: 'else'} ];
+  const totalPages = 2;
+  const pag = new pagination.Pagination(pages, files, totalPages);
+
   let page = new pagination.Page(layout, groupName, pag, path, filename, extension, description);
+
+  t.equals(page.layout, layout, 'layout');
+  t.equals(page.group, groupName, 'group');
+  t.equals(page.pagination, pag, 'pagination');
+  t.equals(page.path, `${path}${filename}${extension}`, 'layout');
+  t.equals(page.permalink, `/${path}`, 'permalink');
+  t.equals(page.page_description, description, 'description');
+
   t.end();
 });
 
-t.test('createPath()', function (t) {
-  t.skip();
-  t.end();
-});
+// t.test('createPath()', function (t) {
+//   t.skip();
+//   t.end();
+// });
